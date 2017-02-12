4e324d27af0b504c9183e95026277661b5a848ba
mikesaprykin <mikesaprykin@gmail.com>
2017-02-12 14:38:06 +0200

    reconstruction

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var engine = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('pages/index')
+    response.render('./index.html')
 });
 
 app.get('/cool', function(request, response) {
