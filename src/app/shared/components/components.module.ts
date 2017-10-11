914af6c000db9093eb567e41993712dc7725bf57
mikesaprykin <mikesaprykin@gmail.com>
2017-10-11 19:25:47 +0300

    feat(side-bar): Added side-bar with menu items, social icons added

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HamburgerComponent } from './hamburger';
+import { SideBarComponent } from './side-bar';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    HamburgerComponent
+    HamburgerComponent,
+    SideBarComponent
   ],
   exports: [
-    HamburgerComponent
+    HamburgerComponent,
+    SideBarComponent
   ]
 })
 export class ComponentsModule { }
